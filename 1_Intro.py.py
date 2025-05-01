2f7f892209b214d671de2359a6474a1c36048637
Harsha2910 <harsha.reddy2910@gmail.com>
2025-05-01 09:59:29 -0400

    Renamed app.py to 1_Intro.py to show 'Intro' in sidebar

---
+++ 1_Intro.py.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="F1 2021 Dashboard", page_icon="🏎️", layout="wide")
+
+st.title("🏎️ From the Grandstands")
+st.markdown("The 2021 FORMULA 1 Championship Key races & The Controversy.")
